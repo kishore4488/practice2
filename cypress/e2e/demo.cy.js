bc0589f7eea01907328b2f98f69c6c6182955427
kishore <kishorebilla1@gmail.com>
2025-07-31 12:09:05 +0530

    baseurl

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,10 +1,14 @@
+const user=Cypress.env("username")
+const pass=Cypress.env("password")
+
 describe('',()=>{
+   
     it('',()=>{
 
-        cy.visit("https://www.saucedemo.com");
+        cy.visit("/");
 
-        cy.get("#user-name").type("standard_user");
-        cy.get("#password").type("secret_sauce");
+        cy.get("#user-name").type("kishore").clear().type(user);
+        cy.get("#password").type(pass);
 
         cy.get("#login-button").click()
     })
